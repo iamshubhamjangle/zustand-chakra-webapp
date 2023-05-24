c6b812e2cd511ba8e6f15847edfa567e57d31111
Shubham Jangle <jangleshubham13@gmail.com>
2023-05-24 15:33:32 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Divider,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card m={5} variant={"filled"}>
+        <CardHeader>
+          <Heading size="md">Counter</Heading>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <Text fontSize="4xl">Count: 0</Text>
+          <Button colorScheme="teal" variant="outline" m={2}>
+            Add
+          </Button>
+          <Button colorScheme="teal" variant="outline" m={2}>
+            Subtract
+          </Button>
+        </CardBody>
+      </Card>
     </div>
   );
 }
