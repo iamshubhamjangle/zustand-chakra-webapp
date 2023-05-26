35a8f7acd9b6ff441098428d4487505c54cea310
Shubham Jangle <jangleshubham13@gmail.com>
2023-05-26 17:34:39 +0530

    updated products page

--- src/Components/Product/ProductList.jsx
+++ src/Components/Product/ProductList.jsx
@@ -72,45 +72,49 @@ const products = [
 
 function ProductList() {
   return (
-    <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={4}>
-      {products.map((product) => (
-        <Box
-          key={product.id}
-          borderWidth="1px"
-          borderRadius="lg"
-          overflow="hidden"
-          boxShadow="md"
-        >
-          <Image
-            src={product.image}
-            alt={product.name}
-            h={200}
-            objectFit="contain"
-          />
+    <div className="center">
+      <div className="container">
+        <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={4}>
+          {products.map((product) => (
+            <Box
+              key={product.id}
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
+              boxShadow="md"
+            >
+              <Image
+                src={product.image}
+                alt={product.name}
+                h={200}
+                objectFit="contain"
+              />
 
-          <Box p={4}>
-            <Text fontSize="lg" fontWeight="bold" mb={2}>
-              {product.name}
-            </Text>
+              <Box p={4}>
+                <Text fontSize="lg" fontWeight="bold" mb={2}>
+                  {product.name}
+                </Text>
 
-            <Flex align="center" mb={2}>
-              <Text fontSize="md" color="gray.600">
-                Price: ${product.price}
-              </Text>
-              <Spacer />
-              <Badge colorScheme="teal" ml={2}>
-                {product.ratings} / 5
-              </Badge>
-              <Text color="gray.600" ml={2}>
-                ({product.reviews} reviews)
-              </Text>
-            </Flex>
+                <Flex align="center" mb={2}>
+                  <Text fontSize="md" color="gray.600">
+                    Price: ${product.price}
+                  </Text>
+                  <Spacer />
+                  <Badge colorScheme="teal" ml={2}>
+                    {product.ratings} / 5
+                  </Badge>
+                  <Text color="gray.600" ml={2}>
+                    ({product.reviews} reviews)
+                  </Text>
+                </Flex>
 
-            {/* Add any other product details you want to display */}
-          </Box>
-        </Box>
-      ))}
-    </Grid>
+                {/* Add any other product details you want to display */}
+              </Box>
+            </Box>
+          ))}
+        </Grid>
+      </div>
+    </div>
   );
 }
 
